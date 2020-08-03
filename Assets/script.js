26ede4175db9256248b7c12e74879712c1333ac6
slowdive <schlickjoe@gmail.com>
2020-08-02 23:23:37 -0700

    index: removed bootstrap color which revealed function color changes - js: added > index to row function to target input section of row - CSS: changed font color for past

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,31 @@
 $("#currentDay").text(moment().format("[Today is] dddd, MMMM Do YYYY"));
 
 var currentHour = moment().hour();
+console.log(currentHour)
+
+$( ".row > input" ).each(function() {
+    var rowHour = $( this ).attr( "id" );
+    console.log(rowHour);
+    if (rowHour > currentHour) {
+        $(this).addClass("future")
+    }
+    else if (rowHour == currentHour) {
+        $(this).addClass("present")
+    }
+    else {
+        $(this).addClass("past")
+    }
+  });
+
+
+
+
+
 var setInput
 var setID
 
-var getInput = window.localStorage.getItem(setInput, setId);
-$("input").val()
+// var getInput = window.localStorage.getItem(setInput, setId);
+// $("input").val(getInput)
 
 $(".saveBtn").on("click", function() {
     var setInput = $(this).siblings("input").val();
