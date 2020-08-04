b75871f617be9ba33c9ae22dfc005bad3593df9b
slowdive <schlickjoe@gmail.com>
2020-08-03 23:34:31 -0700

    tutor session - changed code affected by HTML changes. Added local storage get item commands for each hour row. was informed I have not learned the type of loop required to make this a function yet.

--- Assets/script.js
+++ Assets/script.js
@@ -1,37 +1,43 @@
-$("#currentDay").text(moment().format("[Today is] dddd, MMMM Do YYYY"));
+$(document).ready(function () {
+  $("#currentDay").text(moment().format("[Today is] dddd, MMMM Do YYYY"));
 
-var currentHour = moment().hour();
-console.log(currentHour)
+  var currentHour = moment().hour();
+  console.log(currentHour);
 
-$( ".row > input" ).each(function() {
-    var rowHour = $( this ).attr( "id" );
+  $(".row > textarea").each(function () {
+    var rowHour = $(this).attr("id");
     console.log(rowHour);
+
     if (rowHour > currentHour) {
-        $(this).addClass("future")
-    }
-    else if (rowHour == currentHour) {
-        $(this).addClass("present")
-    }
-    else {
-        $(this).addClass("past")
+      $(this).addClass("future");
+    } else if (rowHour == currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("past");
     }
   });
 
-
-
-
-
-var setInput
-var setID
-
-// var getInput = window.localStorage.getItem(setInput, setId);
-// $("input").val(getInput)
-
-$(".saveBtn").on("click", function() {
-    var setInput = $(this).siblings("input").val();
+  $(".saveBtn").on("click", function () {
+    var setInput = $(this).siblings(".input").val();
     var setId = $(this).parent().attr("id");
     console.log(setInput, setId);
+    window.localStorage.setItem(setId, setInput);
+  });
+
+  $("#hour9 .input").val(localStorage.getItem("hour9"));
+  $("#hour10 .input").val(localStorage.getItem("hour10"));
+  $("#hour11 .input").val(localStorage.getItem("hour11"));
+  $("#hour12 .input").val(localStorage.getItem("hour12"));
+  $("#hour13 .input").val(localStorage.getItem("hour13"));
+  $("#hour14 .input").val(localStorage.getItem("hour14"));
+  $("#hour15 .input").val(localStorage.getItem("hour15"));
+  $("#hour16 .input").val(localStorage.getItem("hour16"));
+  $("#hour17 .input").val(localStorage.getItem("hour17"));
+});
+
+/*$(".saveBtn").on("click", function() {
+    var setInput = $(this).siblings("input").val();
+    var setId = $(this).parent().attr("class");
+    console.log(setInput, setId);
     window.localStorage.setItem(setInput, setId);
-})
-
-
+})*/
